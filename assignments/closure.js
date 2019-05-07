64c3d69c8fa0e4a090f81d188eaada91ccd65fac
Katie Rose <katiefelten@gmail.com>
2019-05-07 16:14:16 -0400

    Updated closure assignment

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,6 +1,14 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
 
+function myName() {
+  var name = "Katie Rose"; // name is a local variable created by init
+  function displayName() { // displayName() is the inner function, a closure
+    console.log(name); // displayName() uses variable declared in the parent function    
+  }
+  displayName();
+}
+myName();
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
